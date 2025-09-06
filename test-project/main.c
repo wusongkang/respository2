2600fb60300663702c2767455e64b5cacf4480cb
gg3 <wk15503072113@163.com>
2025-09-06 08:38:12 +0800

    gg3重置

--- test-project/main.c
+++ test-project/main.c
@@ -3,12 +3,6 @@
 int main(int argc, char const *argv[])
 {
     printf("hello github2");
-    printf("gg2第二次提交");
-    printf("gg3第一次提交");
-    printf("gg2第三次提交");
-    printf("gg3第二次提交");
-    printf("gg3第三次提交");
-    printf("gg2第四次提交");
-    printf("gg2第五次提交");
+    printf("gg3重置");
     return 0;
 }
